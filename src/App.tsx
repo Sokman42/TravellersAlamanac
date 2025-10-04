89cd82d8b30a9c7a9110f9a366ca9283c48f902f
Komalpreet-143 <kau17089@sheridancollege.ca>
2025-10-04 14:54:35 -0400

    Add Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
-import {Box} from './components/Box'
+import { FiUserPlus, FiUser } from 'react-icons/fi';
+import bg from './assets/Camping.svg';
+import './App.css';
 
-function App() {
-
-
+export default function App() {
   return (
-    <>
-      <Box></Box>
-    </>
-  )
-}
+    <div className="hero" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="scrim">
+        <p className="welcome">Welcome</p>
 
-export default App
+        <div className="cards">
+          <button className="glass cta" type="button">
+            <FiUser size={56} aria-hidden />
+            <span>Continue as Guest</span>
+          </button>
+          <button className="glass cta">
+            <FiUserPlus size={56} aria-hidden />
+            <span>Create an Account</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
