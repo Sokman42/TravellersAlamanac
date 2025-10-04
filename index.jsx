f5e20a9af2e0318336aebed3822c161ace05f3ea
Austin Sinclair <austinsinclair42@gmail.com>
2025-10-04 13:35:39 -0400

    Project Initialized

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import baikangYuanVdyasdbhvhcUnsplash12 from "./baikang-yuan-vdyasdbhvhc-unsplash-12.png";
+
+export const Image = () => {
+  return (
+    <div className="w-[9123px] h-[6083px]">
+      <img
+        className="fixed -top-1 -left-1 w-[9131px] h-[6091px] aspect-[1.5] object-cover"
+        alt="Baikang yuan"
+        src={baikangYuanVdyasdbhvhcUnsplash12}
+      />
+    </div>
+  );
+};
---
+++ src/components/Box.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import activityBox from '../assets/activityBox.svg'
+
+export class Box extends Component {
+  render() {
+    return (
+      <div>
+        <img src={activityBox} alt="activity" />
+      </div>
+    )
+  }
+}
+
+export default Box
